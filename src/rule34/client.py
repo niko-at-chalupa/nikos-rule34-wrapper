9502eb01066a3fb98de08e0bce6ca5f85b1e8c1d
niko <181960037+niko-at-chalupa@users.noreply.github.com>
2026-03-14 03:08:39 +0800

    add docstrings to client.py

--- src/rule34/client.py
+++ src/rule34/client.py
@@ -7,6 +7,20 @@ class Client:
         self.USER_ID = user_id
 
     def list_posts(self, tags: str | set[str], /, limit: int = 1000, pid: int | None = None) -> list[Post]:
+        """
+        List posts.
+
+        # Paremeters
+        ---
+        tags : str | set[str]
+            The tags you want to search for while listing.
+            This supports everything you could put in rule34.xxx's search, like "sort:score", "-ai_generated", and whatever else.
+            Pass in an empty string or empty set to get everything.
+        limit : int
+            The limit of posts that will be returned. There is a hard limit of 1000 posts per request.
+        pid : int | None
+            The page number.
+        """
         params = {
             "page": "dapi",
             "s": "post",
@@ -29,6 +43,14 @@ class Client:
         return Post.from_multiple_json(response.content.decode("utf-8"))
 
     def get_post(self, post_id: int) -> Post:
+        """
+        Get a post from its ID.
+
+        # Parameters
+        ---
+        post_id : int
+            The ID of the post you want to get.
+        """
         params = {
             "page": "dapi",
             "s": "post",
@@ -54,4 +76,5 @@ if __name__ == "__main__":
     print(f"took {perf_counter() - start}s")
     for post in posts:
         print(f"FILE URL: {post.file_url}")
+        print(f"ID: {post.post_id}\nPARENT ID: {post.parent_id}")
         print(str(post.tag_info) + "\n---")
