3656ed7cc2441fa308c343806dbff0e8713e0161
niko <181960037+niko-at-chalupa@users.noreply.github.com>
2026-03-20 17:37:06 +0800

    Include docstrings with stubs

--- src/rule34/client.pyi
+++ src/rule34/client.pyi
@@ -10,9 +10,46 @@ class Client:
     API_KEY: Incomplete
     USER_ID: Incomplete
     def __init__(self, api_key: str, user_id: str) -> None: ...
-    def list_posts(self, tags: str | set[str], limit: int = 1000, pid: int | None = None) -> list[Post]: ...
-    def get_post(self, post_id: int) -> Post: ...
-    def download_post(self, post: Post, destination: Path, file_name: str | None = None, file_url: str | None = None) -> None: ...
+    def list_posts(self, tags: str | set[str], limit: int = 1000, pid: int | None = None) -> list[Post]:
+        '''
+        List posts.
+
+        # Parameters
+        ---
+        tags : str | set[str]
+            The tags you want to search for while listing.
+            This supports everything you could put in rule34.xxx\'s search, like "sort:score", "-ai_generated", and whatever else.
+            Pass in an empty string or empty set to get everything.
+        limit : int
+            The limit of posts that will be returned. There is a hard limit of 1000 posts per request.
+        pid : int | None
+            The page number.
+        '''
+    def get_post(self, post_id: int) -> Post:
+        """
+        Get a post from its ID.
+
+        # Parameters
+        ---
+        post_id : int
+            The ID of the post you want to get.
+        """
+    def download_post(self, post: Post, destination: Path, file_name: str | None = None, file_url: str | None = None) -> None:
+        """
+        Download a post's media.
+
+        # Parameters
+        ---
+        post : Post
+            Post to download.
+        destination : Path
+            Directory to where the file will be saved.
+        file_name : str | None
+            Name for the file.
+        file_url : str | None
+            URL to download from. `None` will default to `post.file_url`.
+            You can use stuff like `post.file_url`, `post.preview_url`, & `post.sample_url`.
+        """
     def list_posts_from_pool(self, pool_id: int) -> list[Post]: ...
     def list_posts_from_favorites(self, user: int) -> list[Post]: ...
     def list_post_ids_from_pool(self, pool_id: int) -> list[int]: ...
--- src/rule34/posts.pyi
+++ src/rule34/posts.pyi
@@ -12,6 +12,33 @@ class Rating(Enum):
 
 @dataclass
 class TagInfo:
+    '''
+    Catagorizes tags.
+
+    # Attributes
+    ---
+    general : set[str]
+        General tags that don\'t belong in any other catagory. Most tags *(like "futanari")* fall under this.
+    meta : set[str]
+        Tags that describe special properties of a post, like if they have sound, or their resolution.
+        Examples include:
+            - sound
+            - mp4
+            - video
+            - large_filesize
+            - 3d
+    artists : set[str]
+        Tags that identify the original creator of a post\'s media.
+    characters : set[str]
+        Tags that identify named characters within a post.
+    copyrights : set[str]
+        Tags that identify the source franchise, series, or original work that the post\'s media is derived from, or associated with.
+        Examples include:
+            - vocaloid
+            - utau
+            - doki_doki_literature_club
+            - monitoring_(deco*27)
+    '''
     count: dict[str, int] = field(default_factory=dict)
     general: set[str] = field(default_factory=set)
     meta: set[str] = field(default_factory=set)
@@ -22,6 +49,59 @@ class TagInfo:
     def from_json(cls, tag_info: list[dict]) -> TagInfo: ...
 
 class Post(BaseModel):
+    '''
+    Represents a post on rule34.xxx.
+
+    # Properties
+    ---
+    height : int
+        Height of the post\'s media.
+    score : int
+        Defined by how many people voted "up" on the post.
+    file_url : str
+        URL to the post\'s media.
+    parent_id : int
+        Unique ID to a parent post *(if any)*.
+        If `0`, then it is a top level post *(no parent)*.
+    sample_url : str
+        URL to a compressed version of the post\'s media. 
+        Identical to the post\'s `file_url` if the media is small.
+        If the post\'s media is a video, then this is a still image from that video.
+    sample_width : int
+        Defined by the width of the sample image *(the image from `sample_url`)*.
+    sample_height : int
+        Defined by the height of the sample image *(the image from `sample_url`)*.
+    preview_url : str
+        URL to a super small image of the media.
+    rating : Rating
+        The post\'s rating.
+    tags : set[str]
+        The post\'s tags.
+    post_id : int
+        The unique ID to identify the post.
+    width : int
+        Width of the post\'s media.
+    change : int
+        Unix timestamp to when was the last change to the post.
+    hash : str
+        I have no clue what defines this. I tried the md5 of the post\'s media, but it doesn\'t match up with the hash given to us by the API.
+    owner : str
+        Defined by the username of the uploader.
+    status : str
+        The post\'s status.
+        This can be "active", "deleted", or something else. I have not found anything other than the two in my testing, but the fact it\'s a string and not a boolean leads me to believe there are more possible values.
+    source : str
+        URL to a source.
+    has_notes : bool
+        Weather if notes are attached to the post.
+        Notes are things that get overlayed on top of a post\'s media, shown as transparent white boxes on the site.
+    comment_count : int
+        Defined by how many comments are on the post.
+    post_json : str | None
+        Raw JSON from the response used to get information about the post.
+    tag_info : TagInfo | None
+        Tags represented by a TagInfo object.
+    '''
     height: int
     score: int
     file_url: str
